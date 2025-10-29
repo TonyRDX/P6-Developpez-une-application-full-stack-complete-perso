d337fb98c132bd8f60fe2e482207aee14333e558
TonyCDalvin <83288223+TonyRDX@users.noreply.github.com>
2025-10-29 16:27:18 +0100

    init backend

---
+++ back/src/main/java/com/openclassrooms/mddapi/model/User.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.mddapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users") 
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}
